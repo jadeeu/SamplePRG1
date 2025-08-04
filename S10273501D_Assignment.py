c25c27c44d5f6f3c9f88757d0f6f5517906b2b59
jadeeu <s10273501@connect.np.edu.sg>
2025-08-04 14:34:36 +0800

    My  first assignment commit to GitHub

---
+++ S10273501D_Assignment.py
@@ -0,0 +1,35 @@
+game_state = 'main'
+print("---------------- Welcome to Sundrop Caves! ----------------")
+print("You spent all your money to get the deed to a mine, a small")
+print("  backpack, a simple pickaxe and a magical portal stone.")
+print()
+print("How quickly can you get the 500 GP you need to retire")
+print("  and live happily ever after?")
+print("-----------------------------------------------------------")
+
+def show_town_menu():
+    print()
+    # TODO: Show Day
+    print("----- Sundrop Town -----")
+    print("(B)uy stuff")
+    print("See Player (I)nformation")
+    print("See Mine (M)ap")
+    print("(E)nter mine")
+    print("Sa(V)e game")
+    print("(Q)uit to main menu")
+    print("------------------------")
+
+def show_main_menu():
+    print()
+    print("--- Main Menu ----")
+    print("(N)ew game")
+    print("(L)oad saved game")
+#    print("(H)igh scores")
+    print("(Q)uit")
+    print("------------------")
+
+print('----------------------- Shop Menu -------------------------')
+print('(P)ickaxe upgrade to Level 2 to mine silver ore for 50 GP')
+print('(B)ackpack upgrade to carry 12 items for 20 GP')
+print('(L)eave shop')
+print('-----------------------------------------------------------')
